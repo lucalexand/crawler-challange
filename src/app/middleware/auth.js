762c1edc05880bb7a2b289c05abed08a1ecc4bba
Lucas Lima <lucalexand@gmail.com>
2020-03-25 20:28:31 -0300

    iniciando o projeto já com authentication

---
+++ src/app/middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
+
+module.exports = async (req, res, next) => {
+    const authHeaders = req.headers.authorization;
+
+    if (!authHeaders) {
+        return res.status(401).json({ message: 'Token not provided' });
+    }
+
+    const [, token] = authHeaders.split(' ');
+
+    try {
+        const decoded = await promisify(jwt.verify)(token, process.env.APP_SECRET);
+
+        req.id = decoded.id;
+
+        return next();
+    } catch {
+        return res.status(401).json({ message: 'Token invalid' });
+    }
+};
---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+module.exports = {
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT || 'postgres',
+  storage: "./__tests__/database.sqlite",
+  operatorsAnalises: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
